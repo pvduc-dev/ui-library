450518ff4710f2e9a2da05bf8337b4e820dffdd8
Pv Duc <pvduc.dev@gmail.com>
2021-12-19 16:28:26 +0700

    feat: add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest', 'import'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    "import/export": [0],
+    "max-len": [2, {
+      "code": 200
+    }],
+    "max-lines": [1, { "max": 200 }],
+    "no-console": [0],
+    "semi": [2, "always"],
+    "react/jsx-props-no-spreading": [0],
+    "react/jsx-max-props-per-line": [2, { "maximum": 1 }],
+    "react/jsx-indent-props": [2, 2],
+    "react/jsx-first-prop-new-line": [2, "multiline"],
+    "react/jsx-closing-bracket-location": [2, "tag-aligned"],
+    "import/prefer-default-export": [0]
+  },
+};
---
+++ packages/@pvduc-ui/src/button/Button.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+
+interface ButtonProps {
+  isDisabled: boolean;
+}
+
+const Button: FC<ButtonProps> = ({ children }) => {
+  return (
+    <button
+      role="button"
+      type="submit"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
